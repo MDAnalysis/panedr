6111f863b853d5023b31980e8e88fbca95de8aba
Jonathan Barnoud <jonathan@barnoud.net>
2018-09-20 19:57:00 +0200

    Fix a typo in the install, bump to 0.3.1

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(name='panedr',
       install_requires=['pandas'],
       tests_require=tests_require,
       extras_require={'test': tests_require},
-      package_data={'panedr': ['VERSION, ']},
+      package_data={'panedr': ['VERSION', ]},
       python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       )
