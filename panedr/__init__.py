f0d8624a79dcd254f13705436a0bc9bab28a89e2
Jonathan Barnoud <jonathan@barnoud.net>
2019-01-11 10:54:11 +0100

    Move versioning to pbr
    Sem-ver: feature

--- panedr/__init__.py
+++ panedr/__init__.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .panedr import *
+import pbr.version
+__version__ = pbr.version.VersionInfo('vermouth').release_string()
+del pbr
 
-# It is sometime useful to have access to the version number of a library.
-# This is usually done through the __version__ special attribute.
-# To make sure the version number is consistent between setup.py and the
-# library, we read the version number from the file called VERSION that stays
-# in the module directory.
-import os
-VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
-with open(VERSION_FILE) as infile:
-    __version__ = infile.read().strip()
-del os
+from .panedr import *
--- setup.py
+++ setup.py
@@ -1,45 +1,6 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open('README.rst') as infile:
-    readme = infile.read()
-
-with open('panedr/VERSION') as infile:
-    version = infile.read().strip()
-
-tests_require = ['pytest', 'six']
-
-setup(name='panedr',
-      version=version,
-      description='Read and manipulate Gromacs energy files',
-      long_description=readme,
-      url='https://github.com/jbarnoud/panedr',
-      author='Jonathan Barnoud',
-      author_email='jonathan@barnoud.net',
-      license='LGPL',
-      classifiers=['Development Status :: 4 - Beta',
-                   # Indicate who your project is intended for
-                   'Intended Audience :: Developers',
-                   'Topic :: Scientific/Engineering :: Bio-Informatics',
-                   'Topic :: Scientific/Engineering :: Chemistry',
-                   'Topic :: Scientific/Engineering :: Physics',
-
-                   'License :: OSI Approved :: '
-                     'GNU Lesser General Public License v2 or later (LGPLv2+)',
-
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-
-                   'Operating System :: OS Independent',
-                   ],
-      packages=find_packages(),
-      install_requires=['pandas'],
-      tests_require=tests_require,
-      extras_require={'test': tests_require},
-      package_data={'panedr': ['VERSION', ]},
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
-      )
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
