1cbb32798a85630d85d6676f9a6cc907a0700b65
BFedder <bjarnefeddersen@gmx.net>
2022-06-27 12:31:59 +0100

    fix pbr versioning in panedrlite

--- panedrlite/panedrlite/__init__.py
+++ panedrlite/panedrlite/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pbr.version
-__version__ = pbr.version.VersionInfo('panedr').release_string()
+__version__ = pbr.version.VersionInfo('panedrlite').release_string()
 del pbr
 
 from .panedr import *
