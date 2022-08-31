6bd1e6efff2ff171677bf14f64f400832611ccec
bjarne <bjarnefeddersen@gmx.net>
2022-08-31 10:47:52 +0100

    more PEP8

--- panedr/panedr/__init__.py
+++ panedr/panedr/__init__.py
@@ -3,5 +3,3 @@ from .panedr import edr_to_df, get_unit_dictionary
 import pbr.version
 __version__ = pbr.version.VersionInfo('panedr').release_string()
 del pbr
-
-
--- pyedr/pyedr/__init__.py
+++ pyedr/pyedr/__init__.py
@@ -3,5 +3,3 @@ from .pyedr import edr_to_dict, read_edr, get_unit_dictionary
 import pbr.version
 __version__ = pbr.version.VersionInfo('pyedr').release_string()
 del pbr
-
-
