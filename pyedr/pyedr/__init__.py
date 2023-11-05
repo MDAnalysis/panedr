831557ecac93c2378c4cb277642424f496a6cf85
IAlibay <ialibay@mdanalysis.org>
2023-11-05 18:36:42 +0000

    Update packaging & switch to versioningit

--- pyedr/pyedr/__init__.py
+++ pyedr/pyedr/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from .pyedr import ENX_VERSION, edr_to_dict, read_edr, get_unit_dictionary
-import pbr.version
-__version__ = pbr.version.VersionInfo('pyedr').release_string()
-del pbr
+from importlib.metadata import version
+__version__ = version("pyedr")
 
 # export `ENX_VERSION` for version checking in tests
 # this is not useful for normal use
